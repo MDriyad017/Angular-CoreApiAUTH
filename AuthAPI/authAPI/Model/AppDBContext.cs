a99c457900bf0ffac13b89ebc4d4bdf4fc7c13f1
DEV LAP-03 <jahidulislamriyad934515@gmail.com>
2025-08-26 19:37:50 +0600

    commit on 26_08 _ 7:37pm

---
+++ AuthAPI/authAPI/Model/AppDBContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace authAPI.Model
+{
+    public class AppDBContext : IdentityDbContext
+    {
+    }
+}
---
+++ AuthClient/src/app/user/registration/registration.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './registration.component.html',
+  styles: ``
+})
+export class RegistrationComponent {
+  constructor(public formBuilder: FormBuilder){}
+
+  form = this.formBuilder.group({
+    fullName : [''],
+    email : [''],
+    password : [''],
+    confirmPassword : [''],
+  })
+
+  onSubmit(){
+    console.log(this.form.value);
+  }
+
+}
