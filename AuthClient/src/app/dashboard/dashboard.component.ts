4b0bc89ceca879d70b16e5827472ca5df7989595
DEV LAP-03 <jahidulislamriyad934515@gmail.com>
2025-09-02 18:52:25 +0600

    commit on 02_09 _ 6:52pm

--- AuthClient/src/app/dashboard/dashboard.component.ts
+++ AuthClient/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/services/auth.service';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,12 +10,21 @@ import { AuthService } from '../shared/services/auth.service';
   templateUrl: './dashboard.component.html',
   styles: ``
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
   constructor(
     private router:Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private userService: UserService
   ){}
+  
+  fullName: string = '';
+  ngOnInit(): void {
+    this.userService.getUserProfile().subscribe({
+      next: (res:any) => this.fullName = res.fullName,
+      error:(err:any) => console.log('error while retriving User Profile data\n', err)
+    })
+  }
 
   onLogout(){
     this.authService.deleteToken();
--- AuthClient/src/app/shared/services/auth.service.ts
+++ AuthClient/src/app/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TOKEN_KEY } from '../constant';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +9,25 @@ import { TOKEN_KEY } from '../constant';
 export class AuthService {
 
   constructor(private http:HttpClient) { }
-  baseUrl = 'http://localhost:46126/api';
 
   createUser(formData:any){
-    return this.http.post(this.baseUrl + '/account/signup', formData);
+    return this.http.post(environment.apiBaseUrl + '/account/signup', formData);
   }
 
   signin(formData:any){
-    return this.http.post(this.baseUrl + '/account/signin', formData);
+    return this.http.post(environment.apiBaseUrl+ '/account/signin', formData);
   }
 
   isLoggedIn(){
-    return localStorage.getItem(TOKEN_KEY) != null ? true : false;
+    return this.getToken() != null ? true : false;
   }
 
   saveToken(token:string){
     localStorage.setItem(TOKEN_KEY, token);
+  }
+
+  getToken(){
+    return localStorage.getItem(TOKEN_KEY)
   }
 
   deleteToken(){
